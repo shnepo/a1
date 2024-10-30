9b49a5084619f0007a8c8a07ec4f8d7a1e245700
ianbdehaan <ianbertholdhaan@gmail.com>
2024-10-30 12:17:29 +0100

    optimizations + inversion mutation operator

--- ga.py
+++ ga.py
@@ -8,20 +8,38 @@ distances = np.array([tsp(path) for path in paths])
 min_dist = np.min(distances)
 print(f"initial smallest distance = {min_dist}\ninitiating genetic algorithm\n")
 
+def swap_operator(path):
+    mutation = np.random.choice(len(path), 2, replace = False)
+    path[mutation[0]], path[mutation[1]] = path[mutation[1]], path[mutation[0]]
+    return  path
+
+def inversion_operator(path):
+    mutation = np.random.choice(len(path), 2, replace = False)
+    if mutation[0]>mutation[1]:
+        right_half = np.arange(mutation[0], len(path))
+        left_half = np. arange(0, mutation[1]+1)
+        mutation_idxs = np.append(right_half, left_half)
+    else:
+        mutation_idxs = np.arange(mutation[0],mutation[1])
+    path[mutation_idxs] = path[mutation_idxs[::-1]]
+    return path
+
+    
+
 unchanged_count = 0
 epoche = 1
 while unchanged_count < 1000:
     # best % survives
     survival_rate = 65
-    sorting_idxs = np.argsort(distances)
-    paths = np.unique(paths[sorting_idxs][:n_paths*survival_rate//100],axis=0)
+    _, sorting_idxs = np.unique(distances, return_index=True)
+    paths = paths[sorting_idxs][:n_paths*survival_rate//100]
     mutation_rate = 15 
+    mutation_operator = inversion_operator
     # teenage trurtles
     while len(paths)<n_paths*(survival_rate+mutation_rate)/100:
         idx = np.random.choice(len(paths))
         path_clone = paths[idx].copy()
-        mutation = np.random.choice(len(paths[idx]), 2, replace = False)
-        path_clone[mutation[0]], path_clone[mutation[1]] = path_clone[mutation[1]], path_clone[mutation[0]]
+        path_clone = mutation_operator(path_clone)
         paths = np.append(paths, [path_clone], axis=0)
 
     crossover_rate = 100 - (survival_rate+mutation_rate)
