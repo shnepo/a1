76a4b0bbe65d380723a3dad7e6d020aeee9dc3c5
ianbdehaan <ianbertholdhaan@gmail.com>
2024-10-30 16:11:14 +0100

    improvements in code readability

--- ga.py
+++ ga.py
@@ -1,13 +1,6 @@
 from tsp import *
 import numpy as np
 
-tsp = TSP()
-n_paths = 50
-paths = np.array([np.random.permutation(tsp.dim) for _ in range(n_paths)])
-distances = np.array([tsp(path) for path in paths])
-min_dist = np.min(distances)
-print(f"initial smallest distance = {min_dist}\ninitiating genetic algorithm\n")
-
 def swap_operator(path):
     mutation = np.random.choice(len(path), 2, replace = False)
     path[mutation[0]], path[mutation[1]] = path[mutation[1]], path[mutation[0]]
@@ -16,59 +9,62 @@ def swap_operator(path):
 def inversion_operator(path):
     mutation = np.random.choice(len(path), 2, replace = False)
     if mutation[0]>mutation[1]:
-        right_half = np.arange(mutation[0], len(path))
-        left_half = np. arange(0, mutation[1]+1)
-        mutation_idxs = np.append(right_half, left_half)
+        rightHalf = np.arange(mutation[0], len(path))
+        leftHalf = np. arange(0, mutation[1]+1)
+        mutationIdxs = np.append(rightHalf, leftHalf)
     else:
-        mutation_idxs = np.arange(mutation[0],mutation[1])
-    path[mutation_idxs] = path[mutation_idxs[::-1]]
+        mutationIdxs = np.arange(mutation[0],mutation[1])
+    path[mutationIdxs] = path[mutationIdxs[::-1]]
     return path
 
-    
+def crossoverOperator(path0,path1):
+    start = np.random.choice(len(path0))
+    end = np.random.choice(range(start,len(path0)+1))
+    crossoverString = path0[start:end] 
+    path1 = path1[np.isin(path1, crossoverString, invert=True)]
+    path1 = np.insert(path1, start, crossoverString)
+    return path1
 
-unchanged_count = 0
-epoche = 1
-while unchanged_count < 1000:
-    # best % survives
-    survival_rate = 65
-    _, sorting_idxs = np.unique(distances, return_index=True)
-    paths = paths[sorting_idxs][:n_paths*survival_rate//100]
-    mutation_rate = 15 
-    mutation_operator = inversion_operator
-    # teenage trurtles
-    while len(paths)<n_paths*(survival_rate+mutation_rate)/100:
-        idx = np.random.choice(len(paths))
-        path_clone = paths[idx].copy()
-        path_clone = mutation_operator(path_clone)
-        paths = np.append(paths, [path_clone], axis=0)
-
-    crossover_rate = 100 - (survival_rate+mutation_rate)
-    #perform crossovers in the original surviving paths
-    crossovers = [np.random.choice(n_paths*survival_rate//100,2, replace=False) for _ in range(n_paths*crossover_rate//100)]
-    for crossover in  crossovers:
-        path0 = paths[crossover[0]].copy()
-        path1 = paths[crossover[1]].copy()
-        start = np.random.choice(len(path0))
-        end = np.random.choice(range(start,len(path0)+1))
-        crossover_string = path0[start:end] 
-        path2 = path1.copy()
-        path2 = path2[np.isin(path2, crossover_string, invert=True)]
-        path2 = np.insert(path2, start, crossover_string)
-        paths = np.append(paths, [path2], axis = 0)
+def geneticAlgotithm(nPaths=30,maxUnchangedIterations=1000, survivalRate=65, mutationRate=15, mutationOperator=inversion_operator):
+    tsp = TSP()
+    paths = np.array([np.random.permutation(tsp.dim) for _ in range(nPaths)])
     distances = np.array([tsp(path) for path in paths])
-    new_min = np.min(distances)
-    if not epoche%50:
-        print(f'epoche {epoche}...')
-        print(f'smallest distance = {new_min}')
-    if new_min < min_dist:
-        min_dist = new_min
-        unchanged_count = 0
-    else:
-        unchanged_count += 1
-    epoche += 1
+    minDist = np.min(distances)
+    print(f"initial smallest distance = {minDist}\ninitiating genetic algorithm\n")
+    unchangedCount = 0
+    epoche = 1
+    while unchangedCount < maxUnchangedIterations:
+        # best % survives, unique produces the indexes of distances that produce the sorting of unique items, used to choose the surviving paths
+        _, sortingIdxs = np.unique(distances, return_index=True)
+        paths = paths[sortingIdxs][:nPaths*survivalRate//100]
+        # teenage trurtles
+        while len(paths)<nPaths*(survivalRate+mutationRate)/100:
+            idx = np.random.choice(len(paths))
+            mutatedPath = mutationOperator(paths[idx].copy())
+            paths = np.append(paths, [mutatedPath], axis=0)
+        #perform crossovers in the original surviving paths
+        crossoverRate = 100 - (survivalRate+mutationRate)
+        crossovers = [np.random.choice(nPaths*survivalRate//100,2, replace=False) for _ in range(nPaths*crossoverRate//100)]
+        for crossover in  crossovers:
+            crossoveredPath = crossoverOperator(paths[crossover[0]].copy(),paths[crossover[1]].copy())
+            paths = np.append(paths, [crossoveredPath], axis = 0)
+        #calculate new distances and keep track if improvements are being made
+        distances = np.array([tsp(path) for path in paths])
+        newMin = np.min(distances)
+        if newMin < minDist:
+            minDist = newMin
+            unchangedCount = 0
+        else:
+            unchangedCount += 1
+        #print status once every 50 epochs and update epoche
+        if (not epoche%50):
+            print(f'epoche {epoche}...')
+            print(f'smallest distance = {newMin}')
+        epoche += 1
 
-print(f'smallest distance found = {new_min}')
-best_route = np.argmin(distances)
-with TSP(plot=True) as tsp:
-    tsp.plot_route(paths[best_route], distances[best_route])
+    print(f'smallest distance found = {newMin}')
+    best_route = np.argmin(distances)
+    with TSP(plot=True) as tsp:
+        tsp.plot_route(paths[best_route], distances[best_route])
 
+geneticAlgotithm()
